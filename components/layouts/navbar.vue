b61a1363c79a288ee3080b111ca3b4db1917c301
DESKTOP-008IK9K\arnol <arnold.zephir@prevision.io>
2020-02-13 19:56:50 +0100

    NAVBAR -- Dedicated component for navigation

---
+++ components/layouts/navbar.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: `nav-sidebar`
+}
+</script>
+<template>
+  <navbar>
+    <ul>
+      <li><a href="#">Hier</a></li>
+      <li><a href="#">Avant - hier</a></li>
+      <li><a href="#">Encore avant</a></li>
+      <li><a href="#">Il y a longtemps</a></li>
+    </ul>
+  </navbar>
+</template>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  list-style-type: none;
+}
+
+li {
+  font: Helvetica, Verdana, sans-serif;
+  height: 40px;
+  border-bottom: 1px solid #ccc;
+  flex: 1;
+  align-items: center;
+}
+
+li:last-child {
+  border: none;
+}
+
+li a {
+  text-decoration: none;
+  color: #000;
+}
+</style>
---
+++ layouts/blog.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="container">
+    <header>
+      <h1>Header</h1>
+    </header>
+    <main>
+      <section>
+        <nuxt />
+      </section>
+      <navbar> <nav-sidebar /></navbar>
+    </main>
+    <footer><h2>Footer</h2></footer>
+  </div>
+</template>
+<script>
+import NavSidebar from '~/components/layouts/navbar'
+export default {
+  name: `blog-layout`,
+  components: {
+    NavSidebar
+  }
+}
+</script>
+<style scoped>
+#container {
+  background-color: aquamarine;
+  display: flex;
+  flex-flow: column wrap;
+  height: 100vh;
+}
+
+header {
+  background-color: #777;
+  order: 1;
+  flex: 2;
+}
+
+main {
+  display: flex;
+  flex-flow: row wrap;
+  order: 2;
+  flex: 9;
+  background-color: #aaa;
+}
+
+navbar {
+  background-color: #eee;
+  order: 1;
+  flex: 2;
+}
+
+section {
+  order: 2;
+  flex: 10;
+  background-color: #ccc;
+}
+footer {
+  order: 3;
+  flex: 1;
+  background-color: #555;
+}
+
+ul {
+}
+</style>
