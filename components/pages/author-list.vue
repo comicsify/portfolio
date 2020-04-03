bc5da6dad869014c78ae8dbb8f5a8a01e791883d
DESKTOP-992OTQM\zarno <arnold.zephir@prevision.io>
2020-04-03 19:39:22 +0200

    feat(author-list): new component, author-list

---
+++ components/pages/author-list.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: `AuthorList`,
+  props: {
+    authors: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    Liste d'autrices
+    <ul>
+      <li v-for="author in authors" :key="author.name">
+        <nuxt-link :to="{ name: 'author-name', params: { name: author.id } }">
+          {{ author.name }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+body {
+  background-color: #ff7877;
+}
+</style>
