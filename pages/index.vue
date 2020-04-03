d1f6339ec9e74e975d1a8d50f8aa1677b2438361
DESKTOP-992OTQM\zarno <arnold.zephir@prevision.io>
2020-04-03 19:31:57 +0200

    feat(author-list): iterate over data

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="container">
     <div>
-      <h1>
-        portfolio
-      </h1>
-      <h2>
-        A static portfolio and blog
-      </h2>
-      <div></div>
+      <h1>portfolio</h1>
+      <h2>A static portfolio and blog</h2>
+      <div>
+        <author-list></author-list>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import AuthorList from '~/components/pages/author-list'
 export default {
-  layout: 'blog'
+  layout: 'blog',
+  components: {
+    AuthorList
+  },
+  data() {
+    return {
+      authors: ['jean', 'alice', 'esther', 'pierre']
+    }
+  }
 }
 </script>
 <style>
