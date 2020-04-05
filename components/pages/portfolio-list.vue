e4e12dc421267e847a24301935e080b101e32c25
DESKTOP-992OTQM\zarno <arnold.zephir@prevision.io>
2020-04-05 13:14:36 +0200

    feat: new component for listing  portfolios of an author

---
+++ components/pages/portfolio-list.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: `PortfolioList`,
+  props: {
+    author: {
+      type: String,
+      required: true
+    },
+    portfolios: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    Portfolio de {{ author }} :
+    <ul>
+      <li v-for="portfolio in portfolios" :key="portfolio.slug">
+        <nuxt-link
+          :to="{
+            name: 'author-name-portfolio',
+            params: { name: portfolio.slug }
+          }"
+          >{{ portfolio.title }}</nuxt-link
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+body {
+  background-color: #ff7877;
+}
+</style>
