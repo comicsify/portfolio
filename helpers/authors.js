342b14a40cd47e40db5f940f42fc4262b502fb63
DESKTOP-992OTQM\zarno <arnold.zephir@prevision.io>
2020-04-05 13:17:30 +0200

    feat: slugify the authors name in case they have a rotten name

--- helpers/authors.js
+++ helpers/authors.js
@@ -1,8 +1,10 @@
 const axios = require('axios')
+const cloudinary = require('cloudinary').v2
+
 require('dotenv').config()
+const str = require('./strings')
 
 // Test
-const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -10,6 +12,9 @@ cloudinary.config({
   api_secret: process.env.API_SECRET
 })
 
+/**
+ * List all the authors
+ */
 export async function list() {
   const client = axios.create({
     auth: {
@@ -20,11 +25,29 @@ export async function list() {
 
   const srcUrl = `https://${process.env.API_URL}/${process.env.API_VERSION}/${process.env.CLOUD_NAME}/folders`
   const res = await client.get(srcUrl)
-  const list = res.data.folders.map((folder) => folder.name)
+  const list = res.data.folders.map((folder) => ({
+    name: folder.name,
+    slug: str.slugify(folder.name)
+  }))
   return list
 }
 
+/**
+ * Get the true name of an author form her slug
+ */
+
+export async function name(authorSlug) {
+  const authorList = await list()
+  const candidate = authorList.find((author) => author.slug === authorSlug)
+
+  if (candidate !== undefined) return candidate.name
+  return 'Inconnu(e)'
+}
+
+/**
+ * List the existing author url (for static build)
+ */
 export async function listURL() {
   const authorList = await list()
-  return authorList.map((author) => `/author/${author}`)
+  return authorList.map((author) => `/authors/${author.slug}`)
 }
