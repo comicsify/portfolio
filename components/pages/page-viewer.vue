f4e802da3b43c887d9895f6697f66a5e9a284081
Arnold ZEPHIR <arnold.zephir@prevision.io>
2020-08-29 20:02:59 +0200

    feat: pageviewer component boilerplate

---
+++ components/pages/page-viewer.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: `page-viewer`,
+  props: {
+    src: { type: String, required: true },
+    next: { type: String, required: true },
+    prev: { type: String, required: true }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <img v-bind:src="src" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  border: 1px solid red;
+}
+</style>
