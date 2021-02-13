2fde0631da6ade2738ae988b4ca3e7a48da93bb0
Arnold <arnold.zephir@gmail.com>
2021-02-13 20:45:43 +0100

    [ feat ] order image by filename

--- helpers/series.js
+++ helpers/series.js
@@ -15,15 +15,22 @@ cloudinary.config({
  * @param {string} serieName
  * @description Return the pages of a serie from the serie name
  */
-export async function listPages(seriePath) {
+export async function listPages(seriePath, lastImageFirst = true) {
   const res = await cloudinary.search
     .expression(`folder:"${seriePath}/*"`)
     .max_results(60)
     .execute()
 
-  return res.resources.map((image, idx) => ({
+  const list = res.resources.map((image, idx) => ({
     publicId: image.public_id,
     src: image.secure_url,
+    filename: image.filename,
     id: idx
   }))
+
+  if (lastImageFirst) {
+    return list.slice().sort((a, b) => b.filename.localeCompare(a.filename))
+  }
+
+  return list.slice().sort((a, b) => a.filename.localeCompare(b.filename))
 }
